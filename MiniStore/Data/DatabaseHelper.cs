0967d47d3a2ac060cb0f5c32d4eb538757580ff6
trungkien1511 <banhvankien15@gmail.com>
2025-09-11 16:06:25 +0700

    login

---
+++ MiniStore/Data/DatabaseHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace MiniStore.Data
+{
+    public static class DatabaseHelper
+    {
+        private static readonly string connectionString =
+            @"Server=MSI\SQLEXPRESS1;Database=MiniStore;User Id=sa;Password=Password123!;";
+
+        // Thực thi câu lệnh INSERT/UPDATE/DELETE
+        public static void ExecuteNonQuery(string query, Action<SqlCommand> parameterize)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    parameterize?.Invoke(cmd);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Thực thi câu lệnh trả về 1 giá trị
+        public static object ExecuteScalar(string query, Action<SqlCommand> parameterize)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    parameterize?.Invoke(cmd);
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+    }
+}
---
+++ MiniStore/Views/LoginView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using MiniStore.ViewModels;
+
+
+namespace MiniStore.Views
+{
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+            this.DataContext = new LoginViewModel();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.DataContext is LoginViewModel vm && sender is PasswordBox pb)
+            {
+                vm.Password = pb.Password;
+            }
+        }
+    }
+}
